23f08469e8efa4b7303bce488c0d521e593a549b
Arnold Diaz <yeahbah@outlook.com>
2024-02-16 00:43:54 -0500

    Employees in home page

---
+++ Server/laravel/app/Livewire/SignInPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SignInPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.sign-in-page');
+    }
+}
